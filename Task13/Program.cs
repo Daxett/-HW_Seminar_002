da659bdecb595bceb894f89dfd7d334b362676df
Daxett <Daxett@mail.ru>
2022-06-28 20:47:12 +0300

    add task 13 (ready)

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,1 +1,13 @@
-﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.\
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Задача 13 ");
+
+Console.Write("Введи техзначное число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
